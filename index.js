15a4656f7b70ba447fad926fc7ef4d5e8063f12b
Brian Woodward <brian.woodward@gmail.com>
2016-01-16 16:29:25 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * helper-link-to <https://github.com/helpers/helper-link-to>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
