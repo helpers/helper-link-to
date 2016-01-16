925e700a6dd6baeb599fa6666de54af23a797ddc
Brian Woodward <brian.woodward@gmail.com>
2016-01-16 17:59:02 -0500

    implementation

--- index.js
+++ index.js
@@ -7,6 +7,39 @@
 
 'use strict';
 
-module.exports = function () {
-  // do stuff
+var relativePath = require('relative-dest');
+
+module.exports = function linkTo(key, collectionName) {
+  var name = collectionName;
+  if (typeof name === 'object') {
+    name = null;
+  }
+
+  name = name || 'pages';
+
+  if (!this || typeof this.app === 'undefined') {
+    var msg = 'helper-link-to requires that it is used in an `app` created with `templates`.';
+    console.error(msg);
+    return '';
+  }
+
+  var collection = this.app[name];
+  if (typeof collection === 'undefined') {
+    var msg = 'Invalid collection `' + name + '`.';
+    console.error(msg);
+    return '';
+  }
+
+  var current = this.context.view;
+  var target = collection.getView(key);
+
+  if (!target || !Object.keys(target).length) {
+    var msg = 'Unable target find `' + key + '` in `' + name + '`.';
+    console.error(msg);
+    return '';
+  }
+
+  var fromDest = current.dest || current.path;
+  var targetDest = target.dest || target.path;
+  return relativePath(fromDest, targetDest);
 };
